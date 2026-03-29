ce3310d38cb9983ad09763df03a8e65901f34a5a
gorguimarena <gorguimarena@gmail.com>
2026-03-29 21:16:39 +0000

    test upload

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,8 @@ import magasinRoutes from "./magasin.routes.js";
 import employeRoutes from "./employe.routes.js"
 import venteRoutes from "./vente.routes.js";
 import produitRoutes from "./produit.routes.js";
+import uploadController from "../controllers/upload.controller.js";
+import { upload, multerErrorHandler } from "../middlewares/upload.js";
 
 // Créez le router directement ici
 const router = e.router;
@@ -13,5 +15,11 @@ router.use("/magasins", magasinRoutes);
 router.use("/produits", produitRoutes);
 router.use("/employes", employeRoutes);
 router.use("/ventes", venteRoutes);
+router.post(
+  "/test-upload",
+  upload.single("image"),
+  multerErrorHandler,
+  uploadController.testUpload
+);
 
 export default router;
